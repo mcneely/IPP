54d45e2d7d141ac3ea34a3fcc213fe36a2caff20
Paul L. McNeely <paul@paulmcneely.com>
2017-04-22 12:23:37 -0500

    Extra examples and  documentation.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is a POC and possible start to an Inline Preprocessing Library for PHP it uses Yay as it's preprocessing engine.
 See example.php for an explanation of how to use.
+
+**NOTE:** Most modern IDE's and Syntax Checkers will balk at this.
--- example.php
+++ example.php
@@ -5,18 +5,39 @@ use Yay\Engine;
 require 'vendor/autoload.php';
 
 $test = new Prepocess(new Engine(), __FILE__);
+/*
+ * All code below __halt_compiler() is executed at this point
+ * Code between will be execute after however no variables will propagate.
+ */
 $test->initialize();
 
+//This is required for things to work all Macro code should be below this line.
 __halt_compiler();
+echo "
+<pre>";
 macro {
-unless (···expression) { ···body }
+    unless (···expression) { ···body }
 } >> {
-if (! (···expression)) {
-···body
+    if (! (···expression)) {
+        ···body
+    }
 }
+
+macro {
+    __swap ( T_VARIABLE·A , T_VARIABLE·B ) // swap values between two variables
+} >> {
+    (list(T_VARIABLE·A, T_VARIABLE·B) = [T_VARIABLE·B, T_VARIABLE·A])
 }
 
 $x=0;
 unless ($x === 1) {
-echo "\$x is not 1";
+    echo "\$x is not 1";
 }
+
+$foo = "foo";
+$bar = "bar";
+__swap($foo, $bar);
+
+echo "\r\nFoo: $foo";
+echo "\r\nBar: $bar";
+echo "</pre>";
